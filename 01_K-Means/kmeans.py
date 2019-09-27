9e66817c5673a3adea23506ee43cd1fe5e98bd25
Greg Holste <holste1@kenyon.edu>
2019-09-27 14:36:32 -0400

    Notebook w/ K-Means walkthrough

--- 01_K-Means/kmeans.py
+++ 01_K-Means/kmeans.py
@@ -6,6 +6,7 @@
 #######################################################################
 
 from sklearn.datasets import make_blobs
+from sklearn import cluster
 import matplotlib.pyplot as plt
 import numpy as np
 from copy import deepcopy
@@ -47,7 +48,6 @@ def KMeans(data, K, inits, max_iters=100):
         while np.sum((C_new - C_old)**2) != 0 and counter < max_iters:  # while cluster assignments change...
             C_old = deepcopy(C_new)  # old becomes new from previous iteration
 
-
             # Encoder: assign observations to nearest centroid
             for i in range(data.shape[0]):
                 dists = [np.sum((data[i, :] -  m[k])**2) for k in range(K)]
@@ -67,60 +67,95 @@ def KMeans(data, K, inits, max_iters=100):
         SSE = 0.
         for i in range(data.shape[0]):
             SSE += np.sum((data[i, :] - m[C_new[i]])**2)
+        
+        # SSE = 0.
+        # for k in range(K):
+        #     idxs = np.where(C_new == k)[0]
 
-        SSEs.append(SSE)    
+        #     if idxs.size != 0:
+        #         Nk = data[idxs, :].shape[0]  # num observations in cluster k
+        #         SSE += Nk * np.sum((data[idxs, :] - m[k])**2) # or Nk/data.shape[0]
+
+        SSEs.append(SSE)
         Cs.append(C_new)
         Ms.append(m)
 
     return Cs[np.argmin(SSEs)], SSEs[np.argmin(SSEs)], np.array(Ms[np.argmin(SSEs)])
 
 
-def main():
-    # Create toy dataset with K=3 clusters
-    X, y = make_blobs(n_samples=1000, n_features=2, centers=3, random_state=170)
+# def main():
+#     # Create toy dataset with K=3 clusters
+#     X, y = make_blobs(n_samples=1000, n_features=2, centers=3, random_state=170)
 
-    # Run K-Means with K=3
-    C, SSE, M = KMeans(data=X, K=3, inits=20)
+#     # Run K-Means with K=3
+#     C, SSE, M = KMeans(data=X, K=3, inits=20)
 
-    fig, ax =  plt.subplots(1, 2, figsize=(12,6))
-    ax[0].scatter(X[:, 0], X[:, 1], c=y)
-    ax[0].set_title("Original labeled data")
+#     new_SSE = 0.
+#     for k in range(3):
+#         idxs = np.where(C == k)[0]
 
-    ax[1].scatter(X[:, 0], X[:, 1], c=C, alpha=0.2)
-    ax[1].scatter(M[:, 0], M[:, 1], marker="*", s=200, c="black")
-    ax[1].set_title("Clustered Data (K=3)")
-    ax[1].set_xlabel(f"Within-Cluster Scatter: {round(SSE, 3)}")
-    plt.show()
-
-    # Run K-means for K=1,...,6 and make "elbow plot"
-    Cs = []
-    SSEs = []
-    for k in range(1, 7):
-        C, SSE, _ = KMeans(data=X, K=k, inits=5)
-        Cs.append(C)
-        SSEs.append(SSE)
-
-    fig, ax = plt.subplots(1, 1, figsize=(6,6))
-    ax.scatter(list(range(1, 7)), SSEs)
-    ax.set_xlabel("K")
-    ax.set_ylabel("Within-Cluster Scatter")
-    plt.show()
+#         if idxs.size != 0:
+#             Nk = X[idxs, :].shape[0]  # num observations in cluster k
+#             new_SSE += Nk * np.sum((X[idxs, :] - M[k])**2) # or Nk/data.shape[0]
 
 
-main()
+#     # Run sklearn K-Means with K = 3
+#     sk = cluster.KMeans(n_clusters=3, init="random", n_init=20,
+#                           random_state=0, max_iter=100).fit(X)
+
+#     new_sk_SSE = 0.
+#     for k in range(3):
+#         idxs = np.where(sk.labels_ == k)[0]
+
+#         if idxs.size != 0:
+#             Nk = X[idxs, :].shape[0]  # num observations in cluster k
+#             new_sk_SSE += Nk * np.sum((X[idxs, :] - sk.cluster_centers_[k])**2) # or Nk/data.shape[0]
+
+
+#     fig, ax =  plt.subplots(1, 3, figsize=(12,6))
+#     ax[0].scatter(X[:, 0], X[:, 1], c=y)
+#     ax[0].set_title("Original labeled data")
+
+#     ax[1].scatter(X[:, 0], X[:, 1], c=sk.labels_, alpha=0.2)
+#     ax[1].scatter(sk.cluster_centers_[:, 0], sk.cluster_centers_[:, 1], marker="*", s=200, c="black")
+#     ax[1].set_title("Sklearn Clustered Data (K=3)")
+#     ax[1].set_xlabel(f"Within-Cluster Scatter: {round(sk.inertia_, 3)} \n Weighted W(C): {round(new_sk_SSE, 3)}")
+
+#     ax[2].scatter(X[:, 0], X[:, 1], c=C, alpha=0.2)
+#     ax[2].scatter(M[:, 0], M[:, 1], marker="*", s=200, c="black")
+#     ax[2].set_title("Custom Clustered Data (K=3)")
+#     ax[2].set_xlabel(f"Within-Cluster Scatter: {round(SSE, 3)} \n Weighted W(C): {round(new_SSE, 3)}")
+
+#     plt.show()
+
+    # # Run K-means for K=1,...,6 and make "elbow plot"
+    # SSEs = []
+    # for k in range(1, 7):
+    #     _, SSE, _ = KMeans(data=X, K=k, inits=5)
+    #     SSEs.append(SSE)
+
+    # fig, ax = plt.subplots(1, 1, figsize=(6,6))
+    # ax.scatter(list(range(1, 7)), SSEs)
+    # ax.set_xlabel("K")
+    # ax.set_ylabel("Within-Cluster Scatter")
+    # plt.show()
+
+
+# main()
 
 
 
 
 # ######
 # import pandas as pd
+# import seaborn as sns
 # from mpl_toolkits.mplot3d import Axes3D
 
 # def normalize(vec):
 #   return (vec - np.mean(vec)) / np.std(vec)
 
 
-# data = pd.read_csv("CWR_trait_data.csv")
+# data = pd.read_csv("Data/CWR_trait_data.csv")
 # X = data.drop("Species", axis=1).to_numpy()
 # print(X)
 
@@ -139,6 +174,10 @@ main()
 # fig = plt.figure(figsize=(8, 6))
 # ax = fig.add_subplot(111, projection='3d')
 # ax.scatter(X[:, 0], X[:, 1], X[:, 2], alpha=0.5)
+# ax.set_xlabel(data.columns.values[1])
+# ax.set_ylabel(data.columns.values[2])
+# ax.set_zlabel(data.columns.values[3])
+# ax.set_title(f"CWR Trait Data (n={X.shape[0]})")
 # for i, txt in enumerate(data["Species"].tolist()):
 #   ax.text(X[i, 0], X[i, 1], X[i, 2], txt, 'y', fontsize=6, alpha=0.5)
 
@@ -157,9 +196,36 @@ main()
 
 # Ms = np.array(Ms)
 
+# sk_Ms = []
+# sk_Cs = []
+# sk_SSEs = []
+# for k in range(1, 8):
+#     sk = cluster.KMeans(n_clusters=k, init="random", n_init=30,
+#                         random_state=0, max_iter=100).fit(X)
+    
+#     sk_Ms.append(sk.cluster_centers_)
+#     sk_Cs.append(sk.labels_)
+#     sk_SSEs.append(sk.inertia_)
+
+
 # # Elbow plot
-# ax = sns.scatterplot(list(range(1, 8)), SSEs)
-# ax.set(xlabel = "K", ylabel = "Within-Cluster Scatter")
+# # ax = sns.scatterplot(list(range(1, 8)), SSEs)
+# # ax.set(xlabel = "K", ylabel = "Within-Cluster Scatter")
+# fig, ax = plt.subplots(1, 2, figsize=(12,6))
+# ax[0].scatter(list(range(1, 8)), SSEs)
+# ax[1].scatter(list(range(1, 8)), sk_SSEs)
+
+# plt.show()
+
+# # K=2 results
+# fig = plt.figure(figsize=(8, 6))
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(X[:, 0], X[:, 1], X[:, 2], alpha=0.5, c=Cs[1])
+# ax.scatter(Ms[1][:, 0], Ms[1][:, 1], Ms[1][:, 2], marker="*", s=200, c=[0,1])
+# ax.set_xlabel(data.columns.values[1])
+# ax.set_ylabel(data.columns.values[2])
+# ax.set_zlabel(data.columns.values[3])
+# ax.set_title("Clustered Trait Data (K=2)")
 # plt.show()
 
 # # K=3 results
@@ -170,18 +236,19 @@ main()
 # ax.set_xlabel(data.columns.values[1])
 # ax.set_ylabel(data.columns.values[2])
 # ax.set_zlabel(data.columns.values[3])
+# ax.set_title("Clustered Trait Data (K=3)")
 # plt.show()
 
-# # K=2 results
+# # K=4 results
 # fig = plt.figure(figsize=(8, 6))
 # ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(X[:, 0], X[:, 1], X[:, 2], alpha=0.5, c=Cs[1])
-# ax.scatter(Ms[1][:, 0], Ms[1][:, 1], Ms[1][:, 2], marker="*", s=200, c=[0,1])
-# ax.set_xlabel("Leaf Area per Leaf Dry Mass")
-# ax.set_ylabel("Whole Plant Height")
-# ax.set_zlabel("Diameter at Breast Height")
+# ax.scatter(X[:, 0], X[:, 1], X[:, 2], alpha=0.5, c=Cs[3])
+# ax.scatter(Ms[3][:, 0], Ms[3][:, 1], Ms[3][:, 2], marker="*", s=200, c=[0,1,2,3])
+# ax.set_xlabel(data.columns.values[1])
+# ax.set_ylabel(data.columns.values[2])
+# ax.set_zlabel(data.columns.values[3])
+# ax.set_title("Clustered Trait Data (K=4)")
 # plt.show()
-
 
 
 
