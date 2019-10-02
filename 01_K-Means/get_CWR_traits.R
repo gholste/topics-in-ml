a830f718a9f62956dafc31bea5792d32bfdb3316
Greg Holste <holste1@kenyon.edu>
2019-10-01 22:33:29 -0400

    Use seed mass instead of diameter for CWR trait data.

--- 01_K-Means/get_CWR_traits.R
+++ 01_K-Means/get_CWR_traits.R
@@ -1,6 +1,6 @@
 ## Quick script to get trait data for some crop wild relatives from BIEN ##
 ## Author: Greg Holste                                                   ##
-## Last modified: 9/25/19
+## Last modified: 9/28/19
 
 library(BIEN)
 library(mosaic)
@@ -8,7 +8,7 @@ library(mosaic)
 options(stringsAsFactors = F)
 
 # Read in CWR occurrence records (from previous research)
-tmp <- read.csv("~/Desktop/Spring2019/BIOL385/CWR/Data/01_CWR_species_by_cell.csv")
+tmp <- read.csv("Data/CWR_species_by_cell.csv")
 
 # Get species names, remove underscores
 CWRs <- unique(tmp$Species)
@@ -23,13 +23,11 @@ str(data)
 sort(tally(data$trait_name), increasing = T)
 
 # Find species with 3 common continuous traits
-traits_of_interest <- c("leaf area per leaf dry mass", "whole plant height",
-                        "diameter at breast height (1.3 m)")
+traits_of_interest <- c("leaf area per leaf dry mass", "whole plant height", "seed mass")
 s1 <- unique(with(data, scrubbed_species_binomial[trait_name == traits_of_interest[1]])); s1
 s2 <- unique(with(data, scrubbed_species_binomial[trait_name == traits_of_interest[2]])); s2
 s3 <- unique(with(data, scrubbed_species_binomial[trait_name == traits_of_interest[3]])); s3
 
-# species_of_interest <- intersect(s1, s2); length(species_of_interest)
 species_of_interest <- intersect(s1, intersect(s2, s3)); length(species_of_interest)
 
 # Subset trait data for these ~60 CWRs
@@ -54,6 +52,6 @@ tmp3 <- aggregate(with(mydata, trait_value[trait_name == traits_of_interest[3]])
 df <- merge(tmp1, tmp2, by = "Group.1")
 df <- merge(df, tmp3, by = "Group.1")
 colnames(df) <- c("Species", "Leaf Area per Leaf Dry Mass",
-                  "Whole Plant Height", "Diameter at Breast Height")
+                  "Whole Plant Height", "Seed Mass")
 
 write.csv(df, "Data/CWR_trait_data.csv", row.names = F)
